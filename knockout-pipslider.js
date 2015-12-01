0448332e670d1fa29ce77c4bdd05eaeeb8b53d74
Joe.Warwick@summitinsights.com <joe.warwick@summitinsights.com.au>
2015-12-01 14:21:46 +1100

    allow slide to be cancelled by options.slide

--- knockout-pipslider.js
+++ knockout-pipslider.js
@@ -1,25 +1,25 @@
 ko.bindingHandlers.pipSlider = (function () {
     var setValues = function (array, store) {
-            $.each(array, function (i, o) {
-                if (ko.isObservable(store.values)) {
-                    if (ko.isObservable(store.values()[i])) {
-                        if (store.values()[i]() !== o) store.values()[i](o);
-                    } else store.values()[i] = o;
-                } else {
-                    if (ko.isObservable(store.values[i])) {
-                        if (store.values[i]() !== o) store.values[i](o);
-                    } else store.values[i] = o;
-                }
-            });
-            return array;
-        },
+        $.each(array, function (i, o) {
+            if (ko.isObservable(store.values)) {
+                if (ko.isObservable(store.values()[i])) {
+                    if (store.values()[i]() !== o) store.values()[i](o);
+                } else store.values()[i] = o;
+            } else {
+                if (ko.isObservable(store.values[i])) {
+                    if (store.values[i]() !== o) store.values[i](o);
+                } else store.values[i] = o;
+            }
+        });
+        return array;
+    },
         slide = function (e, ui) {
             if (ko.isObservable(this.values)) {
                 setValues(ui.values, this);
                 this.values.notifySubscribers();
             } else {
-                if (this.values.splice) {
-                    setValues(ui.values, this);
+                if (this.values) {
+                    this.values = setValues(ui.values, this);
                 } else if (ko.isObservable(this.value)) {
                     this.value(ui.values ? setValues(ui.values, this) : ui.value);
                 } else {
@@ -40,7 +40,7 @@ ko.bindingHandlers.pipSlider = (function () {
                 store.values = observable;
                 defaults.values = $.map(ko.unwrap(observable), function (v) { return ko.unwrap(v); });
                 if (ko.isObservable(observable))
-                    observable.subscribe(function () {
+                    observable.subscribe(function (diff) {
                         options.values = $.map(observable(), function (v) { return ko.unwrap(v); });
                         var diff = ko.utils.arrayFilter(ko.utils.compareArrays(observable(), store.values()), function (d) { return d.status != 'retained'; });
                         if (diff.length || $(element).slider('values').length != store.values().length) {
@@ -58,8 +58,12 @@ ko.bindingHandlers.pipSlider = (function () {
                 $(element).slider("destroy");
             });
             $(element).data('store', store);
+            if (typeof options.slide === 'function') {
+                $(element).on('slide', options.slide);
+                delete options.slide;
+                $(element).on('slide', slide.bind(store));
+            }
             $(element).slider(options).slider('pips', $.extend(pipDefaults, pipOptions)).slider('float', floatOptions);
-            $(element).on('slide', slide.bind(store));
         },
         update: function (element, valueAccessor, allBindingsAccessor) {
             var options = allBindingsAccessor().sliderOptions || {};
@@ -77,9 +81,9 @@ ko.bindingHandlers.pipSlider = (function () {
                 defaults.value = ko.unwrap(observable);
             }
             options = $.extend(defaults, options);
+            if (typeof options.slide === 'function') delete options.slide;
             $(element).data('store', store);
             $(element).slider(options).slider('pips', $.extend(pipDefaults, pipOptions)).slider('float', floatOptions);
-            $(element).on('slide', slide.bind(store));
         }
     };
 })();
